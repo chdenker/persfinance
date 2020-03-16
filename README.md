8565700161a3254b916d68b794e1539bb76a15c0
Christopher Denker <mail@chrisden.de>
2020-03-16 13:58:34 +0100

    added an option for creating new database, modified README

--- README.md
+++ README.md
@@ -3,5 +3,37 @@
 
 Call the program with the -h parameter to find out how it works.
 
+Example Usage (the file test.db does not exist before):
+```
+chris@chris-TUXEDO:~/Programming/persfinance$ python3 persfinance.py -c test.db
+chris@chris-TUXEDO:~/Programming/persfinance$ python3 persfinance.py -n test.db
+Date: 2020-03-01
+Amount: 1000
+Category: Income
+Comment (optional): 
+chris@chris-TUXEDO:~/Programming/persfinance$ python3 persfinance.py -n test.db
+Date: 
+Amount: -13.19
+Category: Groceries
+Comment (optional): 
+chris@chris-TUXEDO:~/Programming/persfinance$ python3 persfinance.py -n test.db
+Date: 2020-03-10
+Amount: -4
+Category: Food
+Comment (optional): Döner
+chris@chris-TUXEDO:~/Programming/persfinance$ python3 persfinance.py -p test.db
+1    2020-03-01     1000.00             Income    
+3    2020-03-10       -4.00               Food    Döner
+2    2020-03-16      -13.19          Groceries    
+chris@chris-TUXEDO:~/Programming/persfinance$ python3 persfinance.py -s test.db
+YEAR:                2020
+
+Month:               March     
+Total income:         1000.00
+Total expenses:        -17.19
+Sum:                   982.81
+
+```
+
 Originally written in 2017 as a C++ program using a single CSV file
 serving as a database. Rewritten in 2020 as a Python program using SQLite.
--- persfinance.py
+++ persfinance.py
@@ -4,118 +4,139 @@ import datetime
 import calendar
 
 def add_entry(db_cursor, date: str, amount: int, category: str, comment: str = None):
-	# Try to get the ID of the specified category
-	# TODO Fix % appending (considered unsafe)
-	sql = "SELECT id FROM categories WHERE categories.name == '%s'" % category
-	db_cursor.execute(sql)
-	cat_id = db_cursor.fetchone()
-	if cat_id == None: # Category does not exist in DB yet, add it
-		sql = "INSERT INTO categories VALUES (NULL, '%s')" % category
-		db_cursor.execute(sql)
-		# Now, get the new category ID
-		sql = "SELECT id FROM categories WHERE categories.name == '%s'" % category
-		db_cursor.execute(sql)
-		cat_id = db_cursor.fetchone()
-	cat_id = cat_id[0]	# originally, cat_id is a list - but we only need the first element
+    # Try to get the ID of the specified category
+    # TODO Fix % appending (considered unsafe)
+    sql = "SELECT id FROM categories WHERE categories.name == '%s'" % category
+    db_cursor.execute(sql)
+    cat_id = db_cursor.fetchone()
+    if cat_id == None: # Category does not exist in DB yet, add it
+        sql = "INSERT INTO categories VALUES (NULL, '%s')" % category
+        db_cursor.execute(sql)
+        # Now, get the new category ID
+        sql = "SELECT id FROM categories WHERE categories.name == '%s'" % category
+        db_cursor.execute(sql)
+        cat_id = db_cursor.fetchone()
+    cat_id = cat_id[0]  # originally, cat_id is a list - but we only need the first element
 
-	# Insert new entry into DB
-	sql = "INSERT INTO entries VALUES(NULL,'" + date + "', " + str(amount) + ", " + str(cat_id) + ", '" + comment + "')"
-	db_cursor.execute(sql)
+    # Insert new entry into DB
+    sql = "INSERT INTO entries VALUES(NULL,'" + date + "', " + str(amount) + ", " + str(cat_id) + ", '" + comment + "')"
+    db_cursor.execute(sql)
 
 # Get all category names from the DB as a list of strings
 # indexed by the same index (with offset -1) used in the table of categories in the DB
 def fetch_category_names(db_cursor) -> list:
-	sql = "SELECT * FROM categories"
-	db_cursor.execute(sql)
-	cat_res = db_cursor.fetchall()
-	cat_names = []
-	for cat_pair in cat_res:
-		cat_names.append(cat_pair[1])
-	return cat_names
-	
+    sql = "SELECT * FROM categories"
+    db_cursor.execute(sql)
+    cat_res = db_cursor.fetchall()
+    cat_names = []
+    for cat_pair in cat_res:
+        cat_names.append(cat_pair[1])
+    return cat_names
+    
 def print_all_entries(db_cursor):
-	cat_names = fetch_category_names(db_cursor)
+    cat_names = fetch_category_names(db_cursor)
 
-	# Get all entries from the DB as a list
-	# and print them
-	sql = "SELECT * FROM entries ORDER BY date(entries.date)"
-	db_cursor.execute(sql)
-	entries = db_cursor.fetchall()
-	for entry in entries:
-		entry_id = entry[0]
-		date = entry[1]
-		amount = float(int(entry[2]) / 100.0)
-		category = cat_names[int(entry[3]) - 1]
-		comment = entry[4]
-		print("%d    %s    %8.2f    %15s    %s\n" % ( entry_id, date, amount, category, comment), end="")
+    # Get all entries from the DB as a list
+    # and print them
+    sql = "SELECT * FROM entries ORDER BY date(entries.date)"
+    db_cursor.execute(sql)
+    entries = db_cursor.fetchall()
+    for entry in entries:
+        entry_id = entry[0]
+        date = entry[1]
+        amount = float(int(entry[2]) / 100.0)
+        category = cat_names[int(entry[3]) - 1]
+        comment = entry[4]
+        print("%d    %s    %8.2f    %15s    %s\n" % ( entry_id, date, amount, category, comment), end="")
 
 def print_statistics(db_cursor):
-	year_str = "2020"
-	print("%-20s %s\n" % ("YEAR: ", year_str))
-	for month in range(1, 13):
-		if month >= 1 and month <= 9:
-			month_str = "0" + str(month)
-		else:
-			month_str = str(month)
-		sql = """SELECT  SUM(CASE WHEN amount > 0 THEN amount ELSE 0 END) AS total_month_income,
-                         SUM(CASE WHEN amount < 0 THEN amount ELSE 0 END) AS total_month_expenses,
-                         SUM(amount) AS total_month_sum
-                 FROM    entries
-                 WHERE   strftime("%%Y-%%m", entries.date) == "%s-%s"
+    year_str = "2020"
+    print("%-20s %s\n" % ("YEAR: ", year_str))
+    for month in range(1, 13):
+        if month >= 1 and month <= 9:
+            month_str = "0" + str(month)
+        else:
+            month_str = str(month)
+        sql = """SELECT SUM(CASE WHEN amount > 0 THEN amount ELSE 0 END) AS total_month_income,
+                        SUM(CASE WHEN amount < 0 THEN amount ELSE 0 END) AS total_month_expenses,
+                        SUM(amount) AS total_month_sum
+                 FROM   entries
+                 WHERE  strftime("%%Y-%%m", entries.date) == "%s-%s"
               """ % ( year_str, month_str )
-		db_cursor.execute(sql)
-		values = db_cursor.fetchone()
-		total_month_income = values[0]
-		total_month_expenses = values[1]
-		total_month_sum = values[2]
-		if total_month_income != None or total_month_expenses != None or total_month_sum != None:
-			print("%-20s %-10s" % ( "Month: ", calendar.month_name[month] ))
-			print("%-20s %8.2f" % ( "Total income: ", float(total_month_income) / 100.0 ))
-			print("%-20s %8.2f" % ( "Total expenses: ", float(total_month_expenses) / 100.0 ))
-			print("%-20s %8.2f" % ( "Sum: ", float(total_month_sum) / 100.0 ))
-			print()
+        db_cursor.execute(sql)
+        values = db_cursor.fetchone()
+        total_month_income = values[0]
+        total_month_expenses = values[1]
+        total_month_sum = values[2]
+        if total_month_income != None or total_month_expenses != None or total_month_sum != None:
+            print("%-20s %-10s" % ( "Month: ", calendar.month_name[month] ))
+            print("%-20s %8.2f" % ( "Total income: ", float(total_month_income) / 100.0 ))
+            print("%-20s %8.2f" % ( "Total expenses: ", float(total_month_expenses) / 100.0 ))
+            print("%-20s %8.2f" % ( "Sum: ", float(total_month_sum) / 100.0 ))
+            print()
 
 def new_entry_dialog() -> list:
-	entry_str_list = []
-	datestr = input("Date: ")
-	if datestr == "":
-		datestr = str(datetime.date.today())
-	entry_str_list.append(datestr)
-	amountstr = input("Amount: ")
-	entry_str_list.append(amountstr)
-	categorystr = input("Category: ")
-	entry_str_list.append(categorystr)
-	commentstr = input("Comment (optional): ")
-	entry_str_list.append(commentstr)
-	return entry_str_list
+    entry_str_list = []
+    datestr = input("Date: ")
+    if datestr == "":
+        datestr = str(datetime.date.today())
+    entry_str_list.append(datestr)
+    amountstr = input("Amount: ")
+    entry_str_list.append(amountstr)
+    categorystr = input("Category: ")
+    entry_str_list.append(categorystr)
+    commentstr = input("Comment (optional): ")
+    entry_str_list.append(commentstr)
+    return entry_str_list
+
+def create_database(db_cursor):
+    sql = '''CREATE TABLE categories (
+                id integer,
+                name text NOT NULL,
+                PRIMARY KEY (id)
+             )'''
+    db_cursor.execute(sql)
+    sql = '''CREATE TABLE entries (
+                id integer,
+                date text NOT NULL,
+                amount integer NOT NULL,
+                category integer NOT NULL,
+                comment text,
+                PRIMARY KEY (id),
+                FOREIGN KEY (category) REFERENCES categories(id)
+             )'''
+    db_cursor.execute(sql)
 
 def main():
-	prog_description = "PersFinance | (2017 - 2020) written by Christopher Denker"
-	argparser = argparse.ArgumentParser(description=prog_description)
-	argparser.add_argument("database_path")
-	mut_ex_args_group = argparser.add_mutually_exclusive_group()
-	mut_ex_args_group.add_argument("-n", "--new", help="enter new entry", action="store_true")
-	mut_ex_args_group.add_argument("-p", "--print", help="print all entries", action="store_true")
-	mut_ex_args_group.add_argument("-s", "--statistics", help="print statistics", action="store_true")
-	args = argparser.parse_args()
+    prog_description = "PersFinance | (2017 - 2020) written by Christopher Denker"
+    argparser = argparse.ArgumentParser(description=prog_description)
+    argparser.add_argument("database_path")
+    mut_ex_args_group = argparser.add_mutually_exclusive_group()
+    mut_ex_args_group.add_argument("-c", "--create", help="create a new database (specified database_path will be used)", action="store_true")
+    mut_ex_args_group.add_argument("-n", "--new", help="enter new entry", action="store_true")
+    mut_ex_args_group.add_argument("-p", "--print", help="print all entries", action="store_true")
+    mut_ex_args_group.add_argument("-s", "--statistics", help="print statistics", action="store_true")
+    args = argparser.parse_args()
 
-	db_con = sqlite3.connect(args.database_path)
-	db_cursor = db_con.cursor()
+    db_con = sqlite3.connect(args.database_path)
+    db_cursor = db_con.cursor()
 
-	if args.new:
-		entry_str_list = new_entry_dialog()
-		date = entry_str_list[0]
-		amount = int(float(entry_str_list[1]) * 100)
-		category = entry_str_list[2]
-		comment = entry_str_list[3]
-		add_entry(db_cursor, date, amount, category, comment)
-	elif args.print:
-		print_all_entries(db_cursor)
-	elif args.statistics:
-		print_statistics(db_cursor)
+    if args.new:
+        entry_str_list = new_entry_dialog()
+        date = entry_str_list[0]
+        amount = int(float(entry_str_list[1]) * 100)
+        category = entry_str_list[2]
+        comment = entry_str_list[3]
+        add_entry(db_cursor, date, amount, category, comment)
+    elif args.create:
+        create_database(db_cursor)
+    elif args.print:
+        print_all_entries(db_cursor)
+    elif args.statistics:
+        print_statistics(db_cursor)
 
-	db_con.commit()
-	db_con.close()
+    db_con.commit()
+    db_con.close()
 
 if __name__ == "__main__":
-	main()
+    main()
